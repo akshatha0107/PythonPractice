f8a67e8d95ecdc75b55e733eef763ae1a1d75af7
akshatha0107 <67455033+akshatha0107@users.noreply.github.com>
2021-03-25 17:01:56 +0530

    Create ListPractice.py
    Excercise 3 :
    Take a list, say for example this one:
    
      a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
    
    and write a program that prints out all the elements of the list that are less than 5.
    
    Extras:
    
        Instead of printing the elements one by one, make a new list that has all the elements less than 5 from this list in it and print out this new list.
        Write this in one line of Python.
        Ask the user for a number and return a list that contains only elements from the original list a that are smaller than that number given by the user.

---
+++ ListPractice.py
@@ -0,0 +1,11 @@
+a=[1,1,2,3,4,5,6,7,8,9]
+b=[]
+number=int(input("Enter a number which you want to compare with the original list "))
+for i in a:
+  if(i<number):
+    b.append(i)
+print(b)
+
+   
+   
+

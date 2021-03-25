e3b907ec92c10b9a33be82ddc40888d894a05da5
akshatha0107 <67455033+akshatha0107@users.noreply.github.com>
2021-03-25 12:49:33 +0530

    Create InputFromUser.py

---
+++ InputFromUser.py
@@ -0,0 +1,9 @@
+name = input("What is your name :")
+age = int(input("How old are you? :"))
+year = str((2021-age)+100)
+print(name + " will be 100 years old in " + year)
+number = int(input("How many times do you want to print the message"))
+n=1
+while(n<=number):
+  print(name + " will be 100 years old in  " + year)
+  n = n+1
